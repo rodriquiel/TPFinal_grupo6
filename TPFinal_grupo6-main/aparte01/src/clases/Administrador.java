1065d2d8553f2eb84dbffcee1365762d2b623875
LautaCarron <166958031+LautaCarron@users.noreply.github.com>
2024-04-20 18:34:57 -0300

    Add files via upload

--- TPFinal_grupo6-main/aparte01/src/clases/Administrador.java
+++ TPFinal_grupo6-main/aparte01/src/clases/Administrador.java
@@ -1,19 +1,27 @@
 package clases;
 
-import java.util.ArrayList;
-import java.util.Queue;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class Administrador extends Usuario{
 
-	private ColaChofer colaChofer;
-	private ColaVehiculos colaVehiculos;
+	private LinkedList<Chofer> colaChofer = new LinkedList<>();
+	private PriorityQueue<Vehiculo> colaVehiculos = new PriorityQueue<>();
 	
 	public Administrador(String nombre, String contrasena, String nombreReal) {  //crear clase sistema y hacerla singleton
 			super(nombre,contrasena,nombreReal);
-			this.colaChofer = ColaChofer();
-			this.colaVehiculos = ColaVehiculos();
 	}
   
-
+	public void AgregarChofer(Chofer chofer){
+		this.colaChofer.add(chofer);
+	}
+	public Chofer SacarChofer(){
+		return this.colaChofer.poll();
+	}
+	
+	public void agregaVehiculo(Vehiculo vehiculo) 
+	{
+		this.colaVehiculos.add(vehiculo);
+	}
 }
 
